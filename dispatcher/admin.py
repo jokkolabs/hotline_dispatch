1e3e2a791dd29e5f0c5a8ba6515d383ffeff5f4e
renaud gaudin <rgaudin@gmail.com>
2013-06-25 15:07:30 +0000

    admin for user add/edit

--- dispatcher/admin.py
+++ dispatcher/admin.py
@@ -6,12 +6,53 @@ from __future__ import (unicode_literals, absolute_import,
                         division, print_function)
 
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django import forms
 
 from dispatcher.models import (HotlineEvent, HotlineVolunteer,
                                HotlineResponse, Entity, BlackList, Topics)
 
+
+class UserModificationForm(forms.ModelForm):
+    class Meta:
+        model = HotlineVolunteer
+
+
+class UserCreationForm(forms.ModelForm):
+    class Meta:
+        model = HotlineVolunteer
+        fields = ('email',)
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+
+class CustomUserAdmin(UserAdmin):
+    form = UserModificationForm
+    add_form = UserCreationForm
+    list_display = ("username", "first_name", "last_name", "operator")
+    ordering = ("username",)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'first_name', 'last_name',
+                           'is_superuser', 'is_staff', 'is_active',
+                           'operator', 'phone_number')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password', 'first_name', 'last_name',
+                       'is_superuser', 'is_staff', 'is_active', 'operator')}),
+    )
+
+
+admin.site.register(HotlineVolunteer, CustomUserAdmin)
 admin.site.register(HotlineEvent)
-admin.site.register(HotlineVolunteer)
 admin.site.register(HotlineResponse)
 admin.site.register(Entity)
 admin.site.register(BlackList)
